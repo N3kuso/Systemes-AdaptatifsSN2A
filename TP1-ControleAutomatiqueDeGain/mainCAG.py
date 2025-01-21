8a26769743720f22d4d327bc4b07ba5c18bd9a84
Matthieu PICOT <matthieu.picot83@gmail.com>
2025-01-21 11:28:18 +0100

    add normalized signal

--- TP1-ControleAutomatiqueDeGain/mainCAG.py
+++ TP1-ControleAutomatiqueDeGain/mainCAG.py
@@ -17,7 +17,7 @@ n = 8192 # Longueur du signal d'entrée
 n_bins = 64 # Nombre de point pour l'histogramme
 time = np.arange(0,n)
 
-### Génération  d'un signal
+### Génération  d'un signal ###
 ## Gaussien ##
 gaussien = np.random.normal(0, 0.2, n)
 
@@ -57,3 +57,42 @@ plt.hist(uniforme, bins=n_bins)
 plt.title("Histogramme Uniforme")
 plt.show()
 
+### Génération des signaux normalisé ###
+## Gaussien Normalisé ##
+gaussien_norm = Normalize(gaussien)
+
+plt.subplot(211)
+# Affichage Signal Gaussien Norm
+plt.plot(time, gaussien_norm)
+plt.title("Signal gaussien Normalisé")
+plt.subplot(212)
+# Affichage Histogramme Gaussien Norm
+plt.hist(gaussien_norm, bins=n_bins)
+plt.title("Histogramme gaussien Normalisé")
+plt.show()
+
+## Binaire Normalisé ##
+binaire_norm = Normalize(binaire)
+
+plt.subplot(211)
+# Affichage Signal binaire Norm
+plt.plot(time, binaire_norm)
+plt.title("Signal Binaire Normalisé ")
+plt.subplot(212)
+# Affichage Histogramme Binaire Norm
+plt.hist(binaire_norm, bins=n_bins)
+plt.title("Histogramme Binaire Normalisé")
+plt.show()
+
+## Uniforme Normalisé ##
+uniforme_norm = Normalize(uniforme)
+
+plt.subplot(211)
+# Affichage Signal Uniforme
+plt.plot(time, uniforme_norm)
+plt.title("Signal Uniforme Normalisé")
+plt.subplot(212)
+# Affichage Histogramme Uniforme
+plt.hist(uniforme_norm, bins=n_bins)
+plt.title("Histogramme Uniforme Normalisé")
+plt.show()
