d91e004ae3a4c699c536f7fbc162632673097618
Matthieu PICOT <matthieu.picot83@gmail.com>
2025-01-28 11:50:06 +0100

    Add P2 and P3

--- TP1-ControleAutomatiqueDeGain/mainCAG.py
+++ TP1-ControleAutomatiqueDeGain/mainCAG.py
@@ -13,7 +13,7 @@ from FunctionsCAG import Normalize
 ########################################################
 # P1 :
 ########################################################
-n = 8192 # Longueur du signal d'entrée
+n = 16384 # Longueur du signal d'entrée
 n_bins = 64 # Nombre de point pour l'histogramme
 time = np.arange(0,n)
 
@@ -97,16 +97,129 @@ plt.hist(uniforme_norm, bins=n_bins)
 plt.title("Histogramme Uniforme Normalisé")
 plt.show()
 
+########################################################
+# P2 :
+########################################################
 ### utilisation du CAG ###
 from FunctionsCAG import CAG
 
-sigma = 10
-mu = 0.005
-h_test = CAG(binaire_norm, mu, sigma)
+# Définition des variables sigma2 et mu
+sigma2 = np.power(1,2)
 
-h_tild = np.full(n, sigma)
+# Valeur théorique de h2
+h2_theorical = np.full(n, sigma2)
 
-plt.plot(time, h_tild, "r", label="Valeur théorique")
-plt.plot(time, h_test, "b", label="Valeur estimé")
+## Visualisation de l'évolution du pas d'adaptation pour binaire ##
+mu = np.arange(0.00005, 0.005, 0.0005) # Plage de valeur de mu (binaire) 0 <-> 2
+
+# Affichage du h2 théorique
+plt.plot(time, h2_theorical, "r", label="h2 théorique")
+
+# Boucle pour tester les différentes valeurs de mu
+for i in mu :    
+    h2_experimental, y_experimental = CAG(binaire_norm, i, sigma2)
+    plt.plot(time, h2_experimental, '-', linewidth=1)
+
 plt.legend()
+plt.title("Evolution du coef h2 pour un signal binaire")
+plt.show()
+
+## Visualisation de l'évolution du pas d'adaptation pour gaussien ##
+mu = np.arange(0.00005, 0.005, 0.0005) # Plage de valeur de mu (gaussien)
+
+# Affichage du h2 théorique
+plt.plot(time, h2_theorical, "r", label="h2 théorique")
+
+# Boucle pour tester les différentes valeurs de mu
+for i in mu :    
+    h2_experimental, y_experimental = CAG(gaussien_norm, i, sigma2)
+    plt.plot(time, h2_experimental, '-', linewidth=1)
+
+plt.legend()
+plt.title("Evolution du coef h2 pour un signal gaussien")
+plt.show()
+
+## Visualisation de l'évolution du pas d'adaptation pour uniforme ##
+mu = np.arange(0.00005, 0.005, 0.0005) # Plage de valeur de mu (uniforme)
+
+# Affichage du h2 théorique
+plt.plot(time, h2_theorical, "r", label="h2 théorique")
+
+# Boucle pour tester les différentes valeurs de mu
+for i in mu :    
+    h2_experimental, y_experimental = CAG(uniforme_norm, i, sigma2)
+    plt.plot(time, h2_experimental, '-', linewidth=1)
+
+plt.legend()
+plt.title("Evolution du coef h2 pour un signal uniforme")
+plt.show()
+
+########################################################
+# P3 :
+########################################################
+# Valeur d'alpha pour modifier la puissance du signal
+alpha = 0.46 #round(random.random(),1)
+print(f"Valeur de alpha : {alpha}")
+# Instant où la puissance du signal doit changer
+instant_change = round(n/2)
+
+## Signal binaire ##
+# Création d'un signal binaire non stationnaire
+binaire_not_stationnary = binaire_norm
+binaire_not_stationnary[instant_change:] = alpha * binaire_not_stationnary[instant_change:]
+
+mu = 0.005 # valeur du pas d'adaptation
+
+# Utilisation du CAG
+h2_experimental, y_experimental = CAG(binaire_not_stationnary, mu, sigma2)
+
+# Calcul de la nouvelle valeur théorique de h2
+new_h2_theorical = np.full(n, np.power(sigma2/alpha,2))
+
+# Affichage
+plt.plot(time, h2_theorical, "r", label="h2 théorique")
+plt.plot(time, new_h2_theorical, "r--", label="h2 théorique 2")
+plt.plot(time, h2_experimental, '-', linewidth=1)
+plt.legend()
+plt.title("Evolution du coef h2 pour un signal binaire non stationnaire")
+plt.show()
+
+## Signal gaussien ##
+# Création d'un signal gaussien non stationnaire
+gaussien_not_stationnary = gaussien_norm
+gaussien_not_stationnary[instant_change:] = alpha * gaussien_not_stationnary[instant_change:]
+
+mu = 0.005 # valeur du pas d'adaptation
+
+# Utilisation du CAG
+h2_experimental, y_experimental = CAG(gaussien_not_stationnary, mu, sigma2)
+
+# Calcul de la nouvelle valeur théorique de h2
+new_h2_theorical = np.full(n, np.power(sigma2/alpha,2))
+
+plt.plot(time, h2_theorical, "r", label="h2 théorique")
+plt.plot(time, new_h2_theorical, "r--", label="h2 théorique 2")
+plt.plot(time, h2_experimental, '-', linewidth=1)
+plt.legend()
+plt.title("Evolution du coef h2 pour un signal gaussien non stationnaire")
+plt.show()
+
+## Signal uniforme ##
+# Création d'un signal uniforme non stationnaire
+uniforme_not_stationnary = uniforme_norm
+uniforme_not_stationnary[instant_change:] = alpha * uniforme_not_stationnary[instant_change:]
+
+mu = 0.005 # valeur du pas d'adaptation
+
+# Utilisation du CAG
+h2_experimental, y_experimental = CAG(uniforme_not_stationnary, mu, sigma2)
+
+# Calcul de la nouvelle valeur théorique de h2
+new_h2_theorical = np.full(n, np.power(sigma2/alpha,2))
+
+plt.plot(time, h2_theorical, "r", label="h2 théorique")
+plt.plot(time, new_h2_theorical, "r--", label="h2 théorique 2")
+plt.plot(time, h2_experimental, '-', linewidth=1)
+plt.legend()
+plt.title("Evolution du coef h2 pour un signal uniforme non stationnaire")
 plt.show()
