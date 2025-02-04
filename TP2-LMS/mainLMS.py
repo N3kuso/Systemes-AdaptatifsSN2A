69a32bd78eb0063546fc578a279cebd1b2011546
Nekuso77 <matthieu.picot83@gmail.com>
2025-02-04 20:18:04 +0100

    Revert "add title to Signal plot"
    This reverts commit 3d176d73ef2df04fc839803d5dbb266fe4203072.

--- TP2-LMS/mainLMS.py
+++ TP2-LMS/mainLMS.py
@@ -19,15 +19,14 @@ f = 1000 # Fréquence du signal
 ## Génération du signal Binaire ##
 binary_signal = FunctionLMS.BinarySignal(n)
 # Affichage
-FunctionLMS.PlotSignal(time, binary_signal, title="Signal Binaire")
+FunctionLMS.PlotSignal(time, binary_signal)
 
 ## Génération du signal Gaussien
 gaussian_signal = FunctionLMS.GaussianSignal(n)
 # Affichage
-FunctionLMS.PlotSignal(time, gaussian_signal, title="Signal Gaussien")
+FunctionLMS.PlotSignal(time, gaussian_signal)
 
 ## Génération du signal Sinusoidale ##
 sinusoidal_signal = FunctionLMS.SinusoidalSignal(f, n, time)
 # Affichage
-<<<<<<< HEAD
 FunctionLMS.PlotSignal(time, sinusoidal_signal, title="Signal Sinusoîdal")
